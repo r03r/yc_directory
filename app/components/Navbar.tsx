d2765463da984f9c1ccf9107dc1930c6fe7bd933
r03 <r03r000@gmail.com>
2024-11-01 22:33:48 +0100

    ahora funcionando para implementar font local

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,29 +1,30 @@
-
-import { auth, signIn, signOut } from "@/auth";
-
 import Image from "next/image";
 import Link from "next/link";
+
+import { auth, signIn, signOut } from "@/auth";
 
 const Navbar = async () => {
   const session = await auth();
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.jpeg" alt="logo" width={80} height={60}></Image>
+        </Link>
         <div className="flex items-center gap-5 text-black">
-          <Link  href="/">
-            <Image src="/logo.jpeg" alt="logo" width={80} height={60}></Image>
-          </Link>
           {session && session?.user ? (
             <>
               <Link href="/startup/create">
                 <span>Create </span>
               </Link>
 
-              <form action={async () => {
-                "use server"
+              <form
+                action={async () => {
+                  "use server";
 
-                await signOut( {redirectTo: "/"});
-              }}>
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
                 <button type="submit">Logout</button>
               </form>
 
@@ -32,12 +33,14 @@ const Navbar = async () => {
               </Link>
             </>
           ) : (
-              <form action={async () => {
+            <form
+              action={async () => {
                 "use server";
-                
-              await  signIn( 'github' );
-              }}>
-              <button  type="submit">Login</button>
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
             </form>
           )}
         </div>
