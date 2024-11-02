d0ed862d1652338ef7e967e65b8549796ed774ff
r03 <r03r000@gmail.com>
2024-11-02 03:45:44 +0100

    segunda prueva en vercel ID configuration/user

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = async () => {
                 <button type="submit">Logout</button>
               </form>
 
-              <Link href={`/user/${session?.id}`}>
+              <Link href={`/user/${session?.user  }`}>
                 <span>{session?.user?.name} </span>
               </Link>
             </>
